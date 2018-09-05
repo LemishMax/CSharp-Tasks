5d44bec2fb1a471c7cadd72cb3a4ab079561f504
LemishMax <lemishmax@gmail.com>
2018-09-05 17:10:59 +0300

    Задание 1: Консольная программа, принимающая на вход коэффициенты квадратного уравнения, и считающая корни через дискриминант.

---
+++ Task 1/Task 1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Task_1
+{
+    public class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Данная программа выполняет уравнения вида ax^2+bx+c, где a не равно 0");
+            Console.Write("Введите значение a:");
+            double a;
+            while (true)
+            {
+                a = CheckInput(Console.ReadLine());
+                if (a != 0) {
+                    break;
+                }
+
+                Console.WriteLine("Значение а не должно быть равным 0. Повторите ввод.");
+            }
+
+            Console.Write("Введите значение b:");
+            var b = CheckInput(Console.ReadLine());
+            Console.Write("Введите значение c:");
+            var c = CheckInput(Console.ReadLine());
+            var d = Math.Pow(b, 2) - 4 * a * c;
+            if (d < 0)
+            {
+                Console.WriteLine("У этого уравнения корней нет.");
+            }
+            else
+            {
+                var x1 = (-b + Math.Sqrt(d)) / 2 / a;
+                Console.WriteLine("Результат работы программы:\nx1=" + x1);
+                if (d > 0)
+                {
+                    var x2 = (-b - Math.Sqrt(d)) / 2 / a;
+                    Console.WriteLine("x2=" + x2);
+                }
+
+            }
+
+            Console.ReadKey();
+        }
+
+        private static double CheckInput(string s)
+        {
+            double res;
+            while (true)
+            {
+                if (double.TryParse(s, out res))
+                {
+                    break;
+                }
+
+                Console.WriteLine("Значение введено не верно. Повторите ввод.");
+            }
+
+            return res;
+        }
+    }
+}
