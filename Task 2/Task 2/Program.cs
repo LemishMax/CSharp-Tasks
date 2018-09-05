7da900e284be2488e04761b01bb889e37267c633
LemishMax <lemishmax@gmail.com>
2018-09-05 22:52:45 +0300

    Изменен путь к файлам. Небольшие фиксы

--- Task 2/Task 2/Program.cs
+++ Task 2/Task 2/Program.cs
@@ -8,40 +8,38 @@ namespace Task_2
     {
         private static void Main()
         {
-            Console.WriteLine("Данная программа считает количество уникальных слов в указаных Вами файлах");
-            var fileNames = new List<string>();
+            Console.WriteLine("Данная программа считает количество уникальных слов в указаных Вами файлах. Поместите текстовые файлы в папку textfiles");
             Console.WriteLine("Введите названия файлов через пробел");
-            fileNames.AddRange(Console.ReadLine()?.Split(' '));
-            var uniqueWords= new HashSet<string>();
+            var fileNames = Console.ReadLine()?.Split(' ');
+            var uniqueWords = new HashSet<string>();
             foreach (var name in fileNames)
             {
-                var path = "../../../" + name;
+                var path = Path.Combine("textfiles/", name);
                 if (File.Exists(path))
                 {
-                    using (var sr = new StreamReader(path))
+                    var content = File.ReadAllText(path).ToLower();
+                    var word = "";
+                    foreach (var c in content)
                     {
-                        var content = sr.ReadToEnd().ToLower();
-                        var word = "";
-                        foreach (var c in content)
+                        if (char.IsLetterOrDigit(c))
                         {
-                            if (char.IsLetterOrDigit(c))
-                            {
-                                word += c;
-                            }else if (word.Length != 0 && (char.IsPunctuation(c) || char.IsWhiteSpace(c))){
-                                uniqueWords.Add(word);
-                                word = "";
-                            } 
+                            word += c;
                         }
-
-                        if (word.Length != 0)
+                        else if (word.Length != 0 && (char.IsPunctuation(c) || char.IsWhiteSpace(c)))
                         {
                             uniqueWords.Add(word);
+                            word = "";
                         }
+                    }
+
+                    if (word.Length != 0)
+                    {
+                        uniqueWords.Add(word);
                     }
                 }
                 else
                 {
-                    Console.WriteLine("Файл "+name+" не обнаружен.");
+                    Console.WriteLine($"Файл {name} не обнаружен.");
                 }
             }
 
